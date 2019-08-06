592016d42b8277e6cd28315aba0cd4538e0a990f
songweidong <18510894615@163.com>
2019-08-06 15:02:26 +0800

    ttttttttttttt

--- src/main/java/com/song/controller/AsyncTest.java
+++ src/main/java/com/song/controller/AsyncTest.java
@@ -26,6 +26,7 @@ public class AsyncTest {
         ModelAndView modelAndView = new ModelAndView("view");
         serviceImpl.send();
         modelAndView.addObject("name","I Am Boomking");
+        //yyuioyuio
         //888888
         return modelAndView;
     }
